fb52fb93f5e9812249874ed619641affcf301a71
dongjianzhu <270279215@qq.com>
2021-09-25 10:26:53 +0800

    31121

--- security/src/main/java/com/example/security/SecurityApplication.java
+++ security/src/main/java/com/example/security/SecurityApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.example.security.mapper")
+//@ImportResource(locations = {"classpath:kaptcha.xml"})
 public class SecurityApplication {
 
     public static void main(String[] args) {
