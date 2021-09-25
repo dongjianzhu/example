b236f7f5fe66a4eeed4e1839572d5ff193d5316e
dongjianzhu <270279215@qq.com>
2021-09-25 20:06:49 +0800

    31121

--- security/src/test/java/com/example/security/SecurityApplicationTests.java
+++ security/src/test/java/com/example/security/SecurityApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.security;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootTest
 class SecurityApplicationTests {
 
     @Test
     void contextLoads() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
+        for (int i = 0; i < 3; i++) {
+            String encode = passwordEncoder.encode("1234");
+            System.out.println(encode);
+        }
     }
 
 }
