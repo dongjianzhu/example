9ef73512d6cc2b48c76e659170045d47feac4408
dongjianzhu <270279215@qq.com>
2021-09-26 10:31:15 +0800

    31121

---
+++ gome/src/main/java/com/example/gome/GomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomeApplication.class, args);
+    }
+
+}
---
+++ gome/src/test/java/com/example/gome/GomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
